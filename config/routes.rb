2a4f0183efc0c8aed2ed75bb0b9ff1ef6e6bb56b
Ryan Camp <campryan@comcast.net>
2020-04-04 18:11:37 -0600

    Add ability to click on trip name and be taken to show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  # trips
   get '/trips', to: 'trips#index'
+  get '/trips/:id', to: 'trips#show'
 end
--- spec/features/trips/index_spec.rb
+++ spec/features/trips/index_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "When i visit the hiking trips index page.", type: :feature do
     @trip2 = Trip.create(name:"Worst")
     @trip3 = Trip.create(name:"OK")
   end
+
   it "I can see a list of all hiking trip names." do
     visit "/trips"
 
@@ -14,5 +15,15 @@ RSpec.describe "When i visit the hiking trips index page.", type: :feature do
       expect(page).to have_content(@trip2.name)
       expect(page).to have_content(@trip3.name)
     end
+  end
+
+  it "I can click a trip name and be taken to it's show page" do
+    visit "/trips"
+
+    within(".list") do
+      click_link @trip1.name
+    end
+
+    expect(current_path).to eql("/trips/#{@trip1.id}")
   end
 end
